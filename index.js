453adcdeca53c65ba4674c8f22bf60c28af7074d
Romain LUJAN-TADDEÏ <romain.lujan@outlook.fr>
2020-05-26 12:58:49 +0200

    feature(announce): send a message in the off chan when a member join or leave the discord

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+
 const client = new Discord.Client()
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
@@ -9,5 +10,28 @@ client.on('message', msg => {
   }
 })
 
-// client.login('NTc0MjM2MDc4NTM4NjIwOTI5.XM2xbQ.JOGzv2HcJQt9NG7IXUyXFvRmVQg')
+client.on('guildMemberAdd', member => {
+  let channel = member.guild.channels.find('name', 'officiers');
+  let memberAvatar = member.user.avatarURL;
+      if(!channel) return;
+      let embed = new Discord.RichEmbed()
+          .setColor('WHITE')
+          .setThumbnail(memberAvatar)
+          .addField('ARRIVÉ' , ` ${member} vient de rejoindre le discord`)
+          
+      channel.sendEmbed(embed);
+});
+
+client.on('guildMemberRemove', member => {
+  let channel = member.guild.channels.find('name', 'officiers');
+  let memberAvatar = member.user.avatarURL;
+      if(!channel) return;
+      let embed = new Discord.RichEmbed()
+          .setColor('WHITE')
+          .setThumbnail(memberAvatar)
+          .addField('DÉPART' , `${member} vient de quitter le discord`)
+          
+      channel.sendEmbed(embed);
+});
+
 client.login(process.env.BOT_TOKEN);
